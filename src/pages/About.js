1a77fb3afc05a185511058bb74b692f14a9a6633
Mabunay <mabunaygo@MNLPREPDSN2WSA.local>
2021-01-30 20:12:10 +0800

    Initial Commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h2 className='section-title'>about us</h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+        aspernatur facere officia eaque soluta, iste, est quos odio
+        perspiciatis, sint voluptate et debitis hic totam. Beatae odit velit
+        mollitia maxime aliquid? Aspernatur, provident numquam vitae tempore
+        quisquam dolores eum aliquam.
+      </p>
+    </section>
+  );
+}
+
+export default About
